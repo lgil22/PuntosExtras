7892f047b47d80bf40e5a925af67a9a6deea57d2
Luis David Gil Baez <luis_gil@ucne.edu.do>
2020-03-06 22:10:11 -0400

    Boton Guardado Excelente

---
+++ Convertidor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPersona
+{
+    public static class Convertidor
+    {
+        public static int ToInt(this string valor)
+        {
+            int retorno = 0;
+            int.TryParse(valor, out retorno);
+            return retorno;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using RegistroPersona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RegistroPersona.BLL;
 
 namespace RegistroPersona
 {
@@ -24,5 +18,85 @@ namespace RegistroPersona
         {
             InitializeComponent();
         }
+
+
+        private void Limpiar()
+        {
+            IdTextBox.Text = "0";
+            NombresTextBox.Text = string.Empty;
+       
+        }
+
+        private Persona LlenaClase()
+        {
+            Persona personas = new Persona();
+            personas.PersonaId = Convert.ToInt32(IdTextBox.Text);
+
+            //idTextBox.Text.ToInt();
+            personas.Nombres = NombresTextBox.Text;
+            return personas;
+        }
+
+        private void LlenaCampo(Persona personas)
+        {
+            IdTextBox.Text = Convert.ToString(personas.PersonaId);
+            NombresTextBox.Text = personas.Nombres;
+   
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Persona personas;
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            personas = LlenaClase();
+
+            //Determinar si es guardar o modificar
+            //if (idTextBox.Text.ToInt() == 0)
+
+            if (string.IsNullOrWhiteSpace(IdTextBox.Text) || IdTextBox.Text == "0")
+                paso = PersonaBLL.Guardar(personas);
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede modificar una persona que no existe", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                paso = PersonaBLL.Modificar(personas);
+            }
+
+            //Informar el resultado
+            if (paso)
+                MessageBox.Show("Guardado!!", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("No fue posible guardar!!", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Persona personas = PersonaBLL.Buscar((int)IdTextBox.Text.ToInt());
+            return (personas != null);
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+
+            if (NombresTextBox.Text == string.Empty)
+            {
+                MessageBox.Show(NombresTextBox.Text, "El campo Nombre no puede estar vacio ");
+                NombresTextBox.Focus();
+                paso = false;
+            }
+
+            Persona personas = PersonaBLL.Buscar((int)IdTextBox.Text.ToInt());
+            return paso;
+        }
+
     }
 }
